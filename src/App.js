57dbcb799c5c01bf85a0f8d63eaac30b949139d7
FrandyGuerra <frandyguerra2012@gmail.com>
2021-10-25 20:51:47 +0000

    Este es mi codeList aplicando useState

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Task from './Task'
+import './App.css'
+
+const App =()=>{
+    return (
+        <>
+        <div className="container w-50 bg-light">
+            <h1>todos</h1>
+            <Task/>
+        </div>
+        </>
+    );
+
+}
+
+
+export default App;
+
+
+
---
+++ src/Task.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './task.css'
+
+
+const Task=()=>{
+     const [tarea,setTarea]= useState([]);
+     const [counter,setCounter]=useState(0);
+
+     const addTask=(e)=>{
+        if(e.keyCode===13){
+            setCounter(tarea.length+1); 
+            setTarea([...tarea, e.target.value])  
+            e.target.value=""
+        }   
+    }
+
+    const DeleteItems = (indexItem) => {
+        setTarea((valores) =>
+          valores.filter(( valor, index) => index !== indexItem)
+        );
+        setCounter(tarea.length-1)
+      };
+    return(
+        <>
+        <input id="input" className="p-3" type="text" placeholder="What needs to be done?" onKeyDown={addTask}/>
+        <ul className="list-group">
+        {
+        tarea.map((valor ,index)=>{
+           return (
+               <>
+                <li className="list-group-item task" key={index}>
+                    <div>{valor}</div>
+                    <span className="borrar" onClick={()=>DeleteItems(index)}>X</span>
+                </li>
+                </>
+            )
+        })
+        } 
+        <span className="list-group-item"id="contador">{counter} {"items"}</span>
+        <span className="sombra-1"></span>
+        <span className="sombra-2"></span>
+        </ul>
+        
+        </>
+    )
+
+}
+
+export default Task;
